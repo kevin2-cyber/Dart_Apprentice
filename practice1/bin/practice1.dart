7126d24c6bbff571d9e779c231fb22438277933f
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-02-21 01:25:27 +0000

    Arithmetic operations

--- practice1/bin/practice1.dart
+++ practice1/bin/practice1.dart
@@ -1,3 +1,13 @@
+
+
 void main(List<String> args) {
   print('Hello, Dart Apprentice Reader!');
+  // Addition
+  print(11+1);
+  // Subtraction
+  print(11-1);
+  // Multiplication
+  print(11*2);
+  // Division
+  print(22/2);
 }
