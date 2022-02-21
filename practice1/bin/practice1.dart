d1f52fa8b47eb796c9687b13559c885021ae412f
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-02-21 01:27:44 +0000

    Update practice1.dart

--- practice1/bin/practice1.dart
+++ practice1/bin/practice1.dart
@@ -1,13 +1,11 @@
-
-
 void main(List<String> args) {
   print('Hello, Dart Apprentice Reader!');
   // Addition
-  print(11+1);
+  print(11 + 1);
   // Subtraction
-  print(11-1);
+  print(11 - 1);
   // Multiplication
-  print(11*2);
+  print(11 * 2);
   // Division
-  print(22/2);
+  print(22 / 2);
 }
