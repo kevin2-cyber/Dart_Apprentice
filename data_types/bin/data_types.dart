6feca65c65b4b2c4bc2d011ac3e6c0b038aa769f
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-02-21 14:48:11 +0000

    data types i.e numbers

---
+++ data_types/bin/data_types.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments) {
+  /// numbers
+  // integers
+  int age = 10;
+  age += 15;
+  print(age.isOdd);
+  print(age.isEven);
+  print(3.1459.round());
+  print('I am $age years old ');
+  num car = 20;
+  car = 20.0;
+  print(car.runtimeType);
+}
