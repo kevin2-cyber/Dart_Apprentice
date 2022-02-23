e8d3f77a6929d5cc6a928fc0c663a578d5e81689
kevin2-cyber <keduful@st.ug.edu.gh>
2022-02-23 15:01:59 +0000

    completed challenge 3
    Application of arithmetic operations with constants

--- data_types/bin/data_types.dart
+++ data_types/bin/data_types.dart
@@ -48,4 +48,13 @@ void main(List<String> arguments) {
    print(age);
    age = 30;
    print(age);
+   // Challenge 3
+   const x = 46;
+   const y = 10;
+   const answer1 = (x * 100) + y;
+   print(answer1);
+   const answer2 = (x * 100) + (y * 100);
+   print(answer2);
+   const answer3 = (x * 100) + (y / 10);
+   print(answer3);
 }
