11f893c613b0e5c9901b48ba1b6bdbcc92b5c13f
kevin2-cyber <keduful@st.ug.edu.gh>
2022-02-25 04:04:03 +0000

    checking types at runtime

--- types/bin/types.dart
+++ types/bin/types.dart
@@ -2,7 +2,12 @@ void main(List<String> arguments) {
   /// Type inference and type annotation.
   // type annotation refers to declaring a variable with it's type.
   // type inference refers to declaring a variable with a constant or var keyword but it's inferred at runtime.
-  int myInteger = 10;
-  double myDouble = 3.24;
+  final myInteger = 10;
+  const myDouble = 3.24;
+  num myNumber = 3.14;
   print('Hello world!\n${myDouble.runtimeType}\n${myInteger.runtimeType}');
+  print(myNumber.runtimeType);
+  print(myNumber is double);
+  print(myInteger is double);
+
 }
