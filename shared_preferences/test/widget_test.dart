dfa781c051fe52e480ef7223010ca77b2235f244
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-04 19:11:29 +0000

    Update widget_test.dart

--- shared_preferences/test/widget_test.dart
+++ shared_preferences/test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:recipe/main.dart';
 
-import 'package:shared_preferences/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
