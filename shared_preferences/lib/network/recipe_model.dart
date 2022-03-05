4c1ad572b3489bf52806e1d3302ad29f918b4add
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-05 11:19:53 +0000

    Update recipe_model.dart

--- shared_preferences/lib/network/recipe_model.dart
+++ shared_preferences/lib/network/recipe_model.dart
@@ -4,10 +4,34 @@ part 'recipe_model.g.dart';
 
 @JsonSerializable()
 class APIRecipeQuery {
-  //TODO: Add APIRecipeQuery.fromJson
+  //Add APIRecipeQuery.fromJson
   factory APIRecipeQuery.fromJson(Map<String, dynamic> json) => _$APIRecipeQueryFromJson(json);
   Map<String, dynamic> toJson() => _$APIRecipeQueryToJson(this);
+  @JsonKey(name: 'q')
+  String query;
+  int from;
+  int to;
+  bool more;
+  int counts;
+  List<APIHits> hits;
+
+  APIRecipeQuery({
+    required this.query,
+    required this.from,
+    required this.to,
+    required this.more,
+    required this.counts,
+    required this.hits,
+});
 }
-//TODO: Add @JsonSerializable() class APIHits
+//Add @JsonSerializable() class APIHits
+class APIHits {
+  APIRecipe recipe;
+
+  APIHits({
+    required this.recipe,
+});
+  
+}
 //TODO: Add @JsonSerializable() class APIRecipe
 //TODO: Add @JsonSerializable() class APIIngredients
