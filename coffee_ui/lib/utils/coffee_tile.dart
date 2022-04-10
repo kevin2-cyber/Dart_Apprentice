8c7521ace557dc88f881fe2531a27b7cd7cb7acb
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-10 21:48:30 +0000

    added a GestureDetector

--- coffee_ui/lib/utils/coffee_tile.dart
+++ coffee_ui/lib/utils/coffee_tile.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_ui/pages/details_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -7,88 +8,93 @@ class CoffeeTile extends StatelessWidget {
   final String coffeePrice;
   final String coffeeAdditive;
 
-  const CoffeeTile({
-    Key? key,
-    required this.coffeeImagePath,
-    required this.coffeeName,
-    required this.coffeePrice,
-    required this.coffeeAdditive
-  }) : super(key: key);
+  const CoffeeTile(
+      {Key? key,
+      required this.coffeeImagePath,
+      required this.coffeeName,
+      required this.coffeePrice,
+      required this.coffeeAdditive})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 25.0),
-      child: Container(
-        padding: const EdgeInsets.all(12.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12.0),
-          color: Colors.black54,
-        ),
-        width: 200,
-        child: Column(
-          children: [
-            // coffee image
-            ClipRRect(
-              borderRadius: BorderRadius.circular(12.0),
-              child: Image.asset(
+    return GestureDetector(
+      onTap: (() => Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const DetailsScreen()))),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 25.0),
+        child: Container(
+          padding: const EdgeInsets.all(12.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12.0),
+            color: Colors.black54,
+          ),
+          width: 200,
+          child: Column(
+            children: [
+              // coffee image
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12.0),
+                child: Image.asset(
                   coffeeImagePath,
-                height: 210,
+                  height: 210,
+                ),
               ),
-            ),
-            // coffee name
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    coffeeName,
-                    style: GoogleFonts.bebasNeue(
-                      textStyle: const TextStyle(
-                        fontSize: 20.0,
+              // coffee name
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      coffeeName,
+                      style: GoogleFonts.bebasNeue(
+                        textStyle: const TextStyle(
+                          fontSize: 20.0,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    coffeeAdditive,
-                    style: GoogleFonts.bebasNeue(
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      coffeeAdditive,
+                      style: GoogleFonts.bebasNeue(
                         textStyle: TextStyle(
-                            color: Colors.grey[700],
+                          color: Colors.grey[700],
                         ),
+                      ),
                     ),
-                  ),
-                  // price
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
+                    // price
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
                             '\$$coffeePrice',
-                          style: GoogleFonts.bebasNeue(),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(4),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(6.0),
-                            color: Colors.orange,
+                            style: GoogleFonts.bebasNeue(),
                           ),
+                          Container(
+                            padding: const EdgeInsets.all(4),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(6.0),
+                              color: Colors.orange,
+                            ),
                             child: const Icon(Icons.add),
-                        ),
-                      ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
