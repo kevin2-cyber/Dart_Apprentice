fb040ea660c1c85a34b222425a3db55a4553addf
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-22 12:09:19 +0000

    working on logs

--- shared_preferences/lib/main.dart
+++ shared_preferences/lib/main.dart
@@ -1,13 +1,24 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 
 import 'ui/main_screen.dart';
 
 Future<void> main() async {
-  // TODO: Call _setupLogging()
+  // Call _setupLogging()
+  _setupLogging();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
-// TODO: Add _setupLogging()
+// Add _setupLogging()
+void _setupLogging() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((rec) {
+    if (kDebugMode) {
+      print('${rec.level.name}: ${rec.time}: ${rec.message}');
+    }
+  });
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
--- shared_preferences/lib/network/recipe_service.dart
+++ shared_preferences/lib/network/recipe_service.dart
@@ -4,9 +4,12 @@ import 'package:chopper/chopper.dart';
 import 'package:recipe_finder/network/model_response.dart';
 import 'package:recipe_finder/network/recipe_model.dart';
 
+import 'model_converter.dart';
+part 'recipe_service.chopper.dart';
+
 const String apiKey = '17f4f0eac73106858bc0fee7046a76bc	';
-const String apiID = 'a40e958e';
-const String apiURL = 'https://api.edamam.com/seach';
+const String apiId = 'a40e958e';
+const String apiUrl = 'https://api.edamam.com/seach';
 
 // Add @ChopperApi() here
 @ChopperApi()
@@ -17,6 +20,31 @@ abstract class RecipeService extends ChopperService {
       @Query('from') int from,
       @Query('to') int to,
       );
-  //TODO: Add create()
+  // Add create()
+  static RecipeService create() {
+    // 1
+    final client = ChopperClient(
+      // 2
+      baseUrl: apiUrl,
+      // 3
+      interceptors: [_addQuery, HttpLoggingInterceptor()],
+      // 4
+      converter: ModelConverter(),
+      // 5
+      errorConverter: const JsonConverter(),
+      // 6
+      services: [
+        _$RecipeService(),
+      ],
+    );
+    // 7
+    return _$RecipeService(client);
+  }
 }
-//TODO: Add _addQuery()
+// Add _addQuery()
+Request _addQuery(Request req) {
+  final params = Map<String, dynamic>.from(req.parameters);
+  params['app_id'] = apiId;
+  params['app_key'] = apiKey;
+  return req.copyWith(parameters: params);
+}
