f7d2b59bc4fecf88c6d964666e22ca0fd08d309d
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-23 23:37:54 +0000

    added Consumer widget

--- shared_preferences/lib/ui/shopping/shopping_list.dart
+++ shared_preferences/lib/ui/shopping/shopping_list.dart
@@ -17,23 +17,29 @@ class _ShoppingListState extends State<ShoppingList> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Add Consumer widget
-    return ListView.builder(
-        itemCount: ingredients.length,
-        itemBuilder: (BuildContext context, int index) {
-          return CheckboxListTile(
-            value: checkBoxValues.containsKey(index) && checkBoxValues[index]!,
-            // TODO: Update title to include name
-            title: Text(ingredients[index]),
-            onChanged: (newValue) {
-              if (newValue != null) {
-                setState(() {
-                  checkBoxValues[index] = newValue;
-                });
-              }
-            },
-          );
-        });
+    // Add Consumer widget
+    return Consumer<MemoryRepository>(
+      builder: (context, repository, child) {
+        final ingredients = repository.findAllIngredients();
+        return ListView.builder(
+            itemCount: ingredients.length,
+            itemBuilder: (BuildContext context, int index) {
+              return CheckboxListTile(
+                value: checkBoxValues.containsKey(index) &&
+                    checkBoxValues[index]!,
+                // TODO: Update title to include name
+                title: Text(ingredients[index]),
+                onChanged: (newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      checkBoxValues[index] = newValue;
+                    });
+                  }
+                },
+              );
+            });
+      }
+    );
     // TODO: Add closing brace and parenthesis
   }
 }
