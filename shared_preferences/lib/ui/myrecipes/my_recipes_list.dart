69ac2e27f36c37056fa9c12476818c8b35d69a43
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-23 23:07:48 +0000

    added recipe definition

--- shared_preferences/lib/ui/myrecipes/my_recipes_list.dart
+++ shared_preferences/lib/ui/myrecipes/my_recipes_list.dart
@@ -30,61 +30,67 @@ class _MyRecipesListState extends State<MyRecipesList> {
   }
 
   Widget _buildRecipeList(BuildContext context) {
-// TODO: Add Consumer
-      return ListView.builder(
-        itemCount: recipes.length,
-        itemBuilder: (BuildContext context, int index) {
-          // TODO: Add recipe definition
-          return SizedBox(
-            height: 100,
-            child: Slidable(
-              actionPane: const SlidableDrawerActionPane(),
-              actionExtentRatio: 0.25,
-              child: Card(
-                elevation: 1.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                color: Colors.white,
-                child: Align(
-                  alignment: Alignment.center,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      leading: CachedNetworkImage(
-                        // TODO: Replace imageUrl hardcoding
-                          imageUrl:
-                              'http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html',
-                          height: 120,
-                          width: 60,
-                          fit: BoxFit.cover),
-                      // TODO: Replace title hardcoding
-                      title: const Text('Chicken Vesuvio'),
+// Add Consumer
+      return Consumer<MemoryRepository>(
+        builder: (context, value, child) {
+          recipes = repository.findAllRecipes();
+        },
+        child: ListView.builder(
+          itemCount: recipes.length,
+          itemBuilder: (BuildContext context, int index) {
+            // Add recipe definition
+            final recipe = recipes[index];
+            return SizedBox(
+              height: 100,
+              child: Slidable(
+                actionPane: const SlidableDrawerActionPane(),
+                actionExtentRatio: 0.25,
+                child: Card(
+                  elevation: 1.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  color: Colors.white,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        leading: CachedNetworkImage(
+                          // Replace imageUrl hardcoding
+                            imageUrl:
+                                'http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html',
+                            height: 120,
+                            width: 60,
+                            fit: BoxFit.cover),
+                        // TODO: Replace title hardcoding
+                        title: const Text('Chicken Vesuvio'),
+                      ),
                     ),
                   ),
                 ),
+                actions: <Widget>[
+                  IconSlideAction(
+                      caption: 'Delete',
+                      color: Colors.transparent,
+                      foregroundColor: Colors.black,
+                      iconWidget: const Icon(Icons.delete, color: Colors.red),
+                      // TODO: Update first onTap
+                      onTap: () {})
+                ],
+                secondaryActions: <Widget>[
+                  IconSlideAction(
+                      caption: 'Delete',
+                      color: Colors.transparent,
+                      foregroundColor: Colors.black,
+                      iconWidget: const Icon(Icons.delete, color: Colors.red),
+                      // TODO: Update second onTap
+                      onTap: () {})
+                ],
               ),
-              actions: <Widget>[
-                IconSlideAction(
-                    caption: 'Delete',
-                    color: Colors.transparent,
-                    foregroundColor: Colors.black,
-                    iconWidget: const Icon(Icons.delete, color: Colors.red),
-                    // TODO: Update first onTap
-                    onTap: () {})
-              ],
-              secondaryActions: <Widget>[
-                IconSlideAction(
-                    caption: 'Delete',
-                    color: Colors.transparent,
-                    foregroundColor: Colors.black,
-                    iconWidget: const Icon(Icons.delete, color: Colors.red),
-                    // TODO: Update second onTap
-                    onTap: () {})
-              ],
-            ),
-          );
-        });
+            );
+          }),
+      );
     // TODO: Add final brace and parenthesis
   }
 // Add deleteRecipe() here
