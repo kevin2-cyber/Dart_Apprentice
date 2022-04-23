179a940c5089735116899884351b4fd8e765728c
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-23 16:50:44 +0000

    name

--- shared_preferences/lib/ui/recipes/recipe_list.dart
+++ shared_preferences/lib/ui/recipes/recipe_list.dart
@@ -1,14 +1,16 @@
 import 'dart:math';
+import 'package:chopper/chopper.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../../network/recipe_model.dart';
+
 import '../recipe_card.dart';
 import 'recipe_details.dart';
 import '../widgets/custom_dropdown.dart';
 import '../colors.dart';
+import '../../network/recipe_model.dart';
 import '../../network/recipe_service.dart';
-import 'package:chopper/chopper.dart';
 import '../../network/model_response.dart';
+import '../../data/models/models.dart';
 
 class RecipeList extends StatefulWidget {
   const RecipeList({Key? key}) : super(key: key);
@@ -289,7 +291,16 @@ class _RecipeListState extends State<RecipeList> {
         Navigator.push(
           topLevelContext,
           MaterialPageRoute(builder: (context) {
-            return const RecipeDetails();
+            final detailRecipe = Recipe(
+                label: recipe.label,
+                image: recipe.image,
+                url: recipe.url,
+                calories: recipe.calories,
+                totalTime: recipe.totalTime,
+                totalWeight: recipe.totalWeight);
+            detailRecipe.ingredients =
+                convertIngredients(recipe.ingredients);
+            return RecipeDetails(recipe: detailRecipe);
           }
           ),
         );
