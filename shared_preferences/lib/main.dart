6dd76d384f119c1e670db802f8f101d09849c1b6
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-24 00:22:10 +0000

    added provider to main

--- shared_preferences/lib/main.dart
+++ shared_preferences/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 
 import 'ui/main_screen.dart';
 import 'data/memory_repository.dart';
+import 'mock_service/mock_service.dart';
 
 Future<void> main() async {
   // Call _setupLogging()
@@ -28,9 +29,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<MemoryRepository>(
-      lazy: false,
-      create: (_) => MemoryRepository(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<MemoryRepository>(
+          lazy: false,
+          create: (_) => MemoryRepository(),
+        ),
+        Provider(
+          create: (_) =>
+          MockService()
+            ..create(),
+          lazy: false,
+        ),
+      ],
       child: MaterialApp(
         title: 'Recipes',
         debugShowCheckedModeBanner: false,
