1273c96c8835292e936f7a030d6e5b7f299b8c84
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-24 00:46:34 +0000

    added watch methods

--- shared_preferences/lib/data/repository.dart
+++ shared_preferences/lib/data/repository.dart
@@ -3,20 +3,24 @@ import 'models/models.dart';
 abstract class Repository {
 
   // Add find methods
-  List<Recipe> findAllRecipes();
-  Recipe findRecipeById(int id);
-  List<Ingredient> findAllIngredients();
-  List<Ingredient> findRecipeIngredients(int recipeId);
+  Future<List<Recipe>> findAllRecipes();
+  // Add watch methods
+  Stream<List<Recipe>> watchAllRecipes();
+  Stream<List<Ingredient>> watchAllIngredients();
+
+  Future<Recipe> findRecipeById(int id);
+  Future<List<Ingredient>> findAllIngredients();
+  Future<List<Ingredient>> findRecipeIngredients(int recipeId);
 
   // Add insert methods
-  int insertRecipe(Recipe recipe);
-  List<int> insertIngredients(List<Ingredient> ingredients);
+  Future<int> insertRecipe(Recipe recipe);
+  Future<List<int>> insertIngredients(List<Ingredient> ingredients);
 
   // Add delete methods
-  void deleteRecipe(Recipe recipe);
-  void deleteIngredient(Ingredient ingredient);
-  void deleteIngredients(List<Ingredient> ingredients);
-  void deleteRecipeIngredients(int recipeId);
+  Future<void> deleteRecipe(Recipe recipe);
+  Future<void> deleteIngredient(Ingredient ingredient);
+  Future<void> deleteIngredients(List<Ingredient> ingredients);
+  Future<void> deleteRecipeIngredients(int recipeId);
 
   // Add initializing and closing methods
   Future init();
