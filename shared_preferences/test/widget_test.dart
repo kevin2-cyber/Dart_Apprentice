f79726ba202fff2132c2ab508c166c8a3abd5346
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-08 12:46:36 +0000

    trying to write test

--- shared_preferences/test/widget_test.dart
+++ shared_preferences/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:recipe_finder/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp(repository: null,));
+    await tester.pumpWidget(MyApp(repository: null,));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
