058563364647c5dbd3b0d46cd4309136de5a1aa0
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-19 00:29:06 +0000

    created structure for a 3-tab app

--- cupertino_app/lib/app/home.dart
+++ cupertino_app/lib/app/home.dart
@@ -1,3 +1,6 @@
+import 'package:cupertino_app/app/product_list_tab.dart';
+import 'package:cupertino_app/app/search_tab.dart';
+import 'package:cupertino_app/app/shopping_cart_tab.dart';
 import 'package:flutter/cupertino.dart';
 
 class CupertinoStoreHomePage extends StatelessWidget {
@@ -5,11 +8,50 @@ class CupertinoStoreHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Cupertino Store'),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            label: 'Cart',
+          ),
+        ],
       ),
-      child: SizedBox(),
+      tabBuilder: (context, index) {
+        late final CupertinoTabView returnValue;
+        switch (index) {
+          case 0:
+            returnValue = CupertinoTabView(builder: (context) {
+              return  const CupertinoPageScaffold(
+                child: ProductListTab(),
+              );
+            });
+            break;
+          case 1:
+            returnValue = CupertinoTabView(builder: (context) {
+              return const CupertinoPageScaffold(
+                child: SearchTab(),
+              );
+            });
+            break;
+          case 2:
+            returnValue = CupertinoTabView(builder: (context) {
+              return const CupertinoPageScaffold(
+                child: ShoppingCartTab(),
+              );
+            });
+            break;
+        }
+        return returnValue;
+      },
     );
   }
 }
