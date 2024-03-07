6656ae68e91ecd776560d259459b55e7745427e8
Kelvin Eduful <keduful@st.ug.edu.gh>
2024-03-07 11:22:46 +0000

    initial commit

---
+++ weather/android/app/src/main/java/com/kimikevin/weather/MainActivity.java
@@ -0,0 +1,6 @@
+package com.kimikevin.weather;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ weather/lib/constants.dart
@@ -0,0 +1,3 @@
+class AppConstants {
+  static const API_KEY = '1aad6ac330ca7344f425d70bca8e39d2';
+}
