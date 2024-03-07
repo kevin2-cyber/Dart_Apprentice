d91471b0c725123a420648ba8a9d861b1c0ce301
Kelvin Eduful <keduful@st.ug.edu.gh>
2024-03-07 13:12:11 +0000

    added sample model

--- weathmob/lib/weather.dart
+++ weathmob/lib/weather.dart
@@ -3,12 +3,14 @@ class Weather {
   final String base;
   final String name;
   final int cod;
+  final String description;
 
   const Weather({
     required this.id,
     required this.base,
     required this.name,
-    required this.cod
+    required this.cod,
+    required this.description
 });
 
   // factory Weather.fromJson(Map<String, dynamic> json) {}
