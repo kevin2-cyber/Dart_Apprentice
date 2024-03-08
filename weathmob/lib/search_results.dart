8b6c5c31e4c95d37c3583cb73017a50afb81a912
Kelvin Eduful <keduful@st.ug.edu.gh>
2024-03-08 16:19:01 +0000

    had problem updating UI

--- weathmob/lib/search_results.dart
+++ weathmob/lib/search_results.dart
@@ -52,95 +52,98 @@ class _SearchResultsState extends State<SearchResults> {
         if(snapshot.hasData) {
           return SingleChildScrollView(
             physics: const BouncingScrollPhysics(),
-            child: Column(
-              children: [
-                Image.network(snapshot.data!.iconUrl, height: 200, width: 200, fit: BoxFit.fill,),
-                const SizedBox(
-                  height: 2,
-                ),
-                Text(
-                  snapshot.data!.name,
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-                Text(
-                  '${snapshot.data!.temperatureInfo.temperature}°C',
-                  style: Theme.of(context).textTheme.displaySmall,
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  physics: const BouncingScrollPhysics(),
-                  padding: const EdgeInsets.only(left: 16, bottom: 5),
-                  child: Wrap(
-                    spacing: 5,
-                    children: List.generate(days.length, (index) =>
-                        ChoiceChip(
-                            label: Text(
-                                days[index],
-                              style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                                color:  Colors.black
+            child: Container(
+              color: const Color(0xFFEBEBEB),
+              child: Column(
+                children: [
+                  Image.network(snapshot.data!.iconUrl, height: 200, width: 200, fit: BoxFit.fill,),
+                  const SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    snapshot.data!.name,
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  Text(
+                    '${snapshot.data!.temperatureInfo.temperature}°C',
+                    style: Theme.of(context).textTheme.displaySmall,
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    physics: const BouncingScrollPhysics(),
+                    padding: const EdgeInsets.only(left: 16, bottom: 5),
+                    child: Wrap(
+                      spacing: 5,
+                      children: List.generate(days.length, (index) =>
+                          ChoiceChip(
+                              label: Text(
+                                  days[index],
+                                style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                                  color:  Colors.black
+                                ),
                               ),
+                            selected: selectedIndex == index,
+                            selectedColor: selectedIndex == index ? Colors.white : const Color(0xFFEBEBEB),
+                            labelPadding: const EdgeInsets.all(2.0),
+                            showCheckmark: false,
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            elevation: selectedIndex == index ? 5 : 0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: const BorderRadius.all(Radius.circular(10)),
+                              side: selectedIndex == index ? const BorderSide(color: Colors.transparent) : const BorderSide(color: Color(0xFFEBEBEB), style: BorderStyle.none),
                             ),
-                          selected: selectedIndex == index,
-                          selectedColor: Colors.white38,
-                          labelPadding: const EdgeInsets.all(2.0),
-                          showCheckmark: false,
-                          padding: const EdgeInsets.symmetric(horizontal: 20),
-                          elevation: selectedIndex == index ? 5 : 0,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            side: BorderSide(color: Colors.transparent),
+                            onSelected: (value) {
+                                setState(() {
+                                  selectedIndex = value ? index : selectedIndex;
+                                });
+                            },
                           ),
-                          onSelected: (value) {
-                              setState(() {
-                                selectedIndex = value ? index : selectedIndex;
-                              });
-                          },
-                        ),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(left: 16),
-                  child: Row(
-                    children: [
-                      Text(
-                          'Locations',
-                        textAlign: TextAlign.start,
-                      ),
-                    ],
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
                   ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-                const LocationCard(),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-                const LocationCard(),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-                const LocationCard(),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-                const LocationCard(),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-                const LocationCard(),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-                const LocationCard(),
-              ],
+                  const Padding(
+                    padding: EdgeInsets.only(left: 16),
+                    child: Row(
+                      children: [
+                        Text(
+                            'Locations',
+                          textAlign: TextAlign.start,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  const LocationCard(),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  const LocationCard(),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  const LocationCard(),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  const LocationCard(),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  const LocationCard(),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  const LocationCard(),
+                ],
+              ),
             ),
           );
         }
@@ -173,7 +176,7 @@ class LocationCard extends StatelessWidget {
           height: MediaQuery.of(context).size.height * 0.1,
           width: MediaQuery.of(context).size.width,
           decoration: const BoxDecoration(
-            color: Colors.black12,
+            color: Color(0xFFF5F5F5),
           ),
           child: const ListTile(
             title: Text('32°C'),
